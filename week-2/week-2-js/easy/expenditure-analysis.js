fa8b1475c3396874d04bbb83d749e58b63f7fff9
avdhut <avdhut@avdhut.tech>
2025-07-18 21:28:07 +0530

    Solved (with hints) expenditure-analysis

--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -14,7 +14,25 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  var { category } = transactions;
+  var categoryMap = {};
+  var arr = [];
+  transactions.forEach((obj) => {
+    if (obj.category in categoryMap) {
+      categoryMap[obj.category] += obj.price;
+    } else {
+      categoryMap[obj.category] = obj.price;
+    }
+  });
+
+  for (category in categoryMap) {
+    arr.push({
+      category: category,
+      totalSpent: categoryMap[category],
+    });
+  }
+
+  return arr;
 }
 
 module.exports = calculateTotalSpentByCategory;
