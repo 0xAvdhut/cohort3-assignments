b064b98729e2c24642c1e8db2ba1e105cdc4fbb9
avdhut <avdhut@avdhut.tech>
2025-07-18 20:18:19 +0530

    Solved easy-anagram

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,10 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let arr = str1.toLowerCase().split("").sort().join("");
+  let arr2 = str2.toLowerCase().split("").sort().join("");
+  let bool1 = arr === arr2 ? true : false;
+  return bool1;
 }
-
+module.exports = isAnagram;
 module.exports = isAnagram;
